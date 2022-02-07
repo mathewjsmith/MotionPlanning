26dd38688bf893c8bc84ede08aebceb6be9d1c95
Mathew Smith <mtjs@protonmail.com>
2022-02-07 23:24:34 +0100

    GA-CBS benchmark results

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for filename in instances/new/rand*n00{4..9}*d8x8*; do
+for filename in instances/new/rand*n0{20..32}*d8x8*; do
      timeout -s SIGINT 10m julia 'benchmarks/benchmark_gacbs.jl' $filename
     #  julia -p 64 'benchmarks/benchmark_constraintga.jl' $filename
 done
